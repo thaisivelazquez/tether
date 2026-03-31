e6b6745cddbc4fb1293fdca8a5a4025a68159cca
thaisivelazquez <thaisivelazquez@gmail.com>
2026-03-30 20:01:49 -0400

    tut page, not interactive, just imge carousel complete

--- frontend/src/app/tut/index.tsx
+++ frontend/src/app/tut/index.tsx
@@ -1,30 +1,42 @@
-import React, { useState } from "react";
-import {
-  Image,
-  Pressable,
-  View,
-  useWindowDimensions,
-} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, Pressable, useWindowDimensions, View } from "react-native";
 import { styles } from "../../../components/tut/tutstyles";
 
-const SLIDES = [
-  require("../../../components/tut/slide1.png"),
-  require("../../../components/tut/slide5.png"),
-  require("../../../components/tut/slide4.png"),
-  require("../../../components/tut/slide3.png"),
-  
-];
+// Assume these are imported correctly as React components
+import Slide1 from "../../../components/tut/slide1.svg";
+import Slide2 from "../../../components/tut/slide2.svg";
+import Slide3 from "../../../components/tut/slide3.svg";
+import Slide4 from "../../../components/tut/slide4.svg";
+import Slide5 from "../../../components/tut/slide5.svg";
+import Slide6 from "../../../components/tut/slide6.svg";
+import Slide7 from "../../../components/tut/slide7.svg";
+
+const SLIDES = [Slide1, Slide2, Slide7, Slide6, Slide3, Slide5, Slide4];
 
 export default function TutPage() {
   const { width, height } = useWindowDimensions();
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
   const handleTap = () => {
     if (currentSlide < SLIDES.length - 1) {
+      // Reset opacity to 0 before showing next slide
+      fadeAnim.setValue(0);
       setCurrentSlide((prev) => prev + 1);
-    } else {
     }
   };
+
+  // Animate opacity every time the slide changes
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 400,
+      useNativeDriver: true,
+    }).start();
+  }, [currentSlide]);
+
+  const CurrentSlide = SLIDES[currentSlide];
 
   return (
     <Pressable style={styles.screen} onPress={handleTap}>
@@ -37,14 +49,9 @@ export default function TutPage() {
           backgroundColor: "#f5f5f0",
         }}
       >
-        <Image
-          source={SLIDES[currentSlide]}
-          style={{
-            width,
-            height,
-          }}
-          resizeMode="contain"
-        />
+        <Animated.View style={{ opacity: fadeAnim, width, height }}>
+          <CurrentSlide width={width} height={height} />
+        </Animated.View>
       </View>
     </Pressable>
   );
